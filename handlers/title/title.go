d266dea9c5d0d7b78aa2ff60911a243a44d50027
kunarawra <kunarawra@gmail.com>
2026-09-19 07:51:51 -0400

    fix handlers title json

--- handlers/title/title.go
+++ handlers/title/title.go
@@ -2,12 +2,14 @@ package title
 
 import (
 	"fmt"
+	"os"
 
 	"msattack/config"
 	"msattack/errors"
 	"msattack/managers"
 	"msattack/utils"
 
+	"github.com/bytedance/sonic"
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog/log"
 )
@@ -31,7 +33,6 @@ func GetPackInfo(c *fiber.Ctx) error {
 	}
 
 	c.Set("Connection", "close")
-	c.Set("Content-Encoding", "gzip")
 	c.Set("Server", "Apache")
 	c.Set("Vary", "Accept-Encoding")
 
@@ -56,7 +57,6 @@ func GetFileList(c *fiber.Ctx) error {
 	}
 
 	c.Set("Connection", "close")
-	c.Set("Content-Encoding", "gzip")
 	c.Set("Server", "Apache")
 	c.Set("Vary", "Accept-Encoding")
 
@@ -64,15 +64,61 @@ func GetFileList(c *fiber.Ctx) error {
 }
 
 func GetMasterTable(c *fiber.Ctx) error {
-    tableNames := c.Context().QueryArgs().PeekMulti("table[]")
-    for _, tableName := range tableNames {
-        log.Info().Msgf("GET /title/get_master_table?table[]=%s", tableName)
-    }
-    err := c.Status(fiber.StatusOK).JSON(fiber.Map{
-        "error_code": 0,
-    })
-    if err != nil {
-        log.Fatal().Err(err).Msg("Error in getting master table data.")
-    }
-    return err
+	// Since we can potentially have multiple "table[]" query parameters, we iterate over them to get the full list
+	tableNames := c.Context().QueryArgs().PeekMulti("table[]")
+
+	requestedTables := make([]string, 0, len(tableNames))
+	for _, tableName := range tableNames {
+		name := string(tableName)
+		requestedTables = append(requestedTables, name)
+		log.Info().Msgf("GET /title/get_master_table?table[]=%s", name)
+	}
+
+	configuration := config.GlobalConfig
+
+	rawBytes, readErr := os.ReadFile(configuration.MasterTableFilename)
+	if readErr != nil {
+		log.Error().Err(readErr).Msg("Failed to read master table file.")
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"response": utils.GenerateErrorCode(errors.MAINTENANCE),
+		})
+	}
+
+	// The master table file is expected to be a JSON object keyed by table name,
+	// e.g. {"chara_m": [...], "weapon_m": [...], ...}
+	var fullTable map[string]sonic.NoCopyRawMessage
+	if unmarshalErr := sonic.Unmarshal(rawBytes, &fullTable); unmarshalErr != nil {
+		log.Error().Err(unmarshalErr).Msg("Failed to unmarshal master table file.")
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"response": utils.GenerateErrorCode(errors.MAINTENANCE),
+		})
+	}
+
+	// If specific tables were requested, only return those; otherwise return everything.
+	responseTable := fullTable
+	if len(requestedTables) > 0 {
+		responseTable = make(map[string]sonic.NoCopyRawMessage, len(requestedTables))
+		for _, name := range requestedTables {
+			if data, ok := fullTable[name]; ok {
+				responseTable[name] = data
+			} else {
+				log.Warn().Msgf("Requested table %q not found in master table file.", name)
+			}
+		}
+	}
+
+	err := c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"master_ver": configuration.MasterVersion,
+		"response":   utils.GenerateErrorCodeWithTime(errors.SUCCESS),
+		"table":      responseTable,
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error in getting master table data.")
+	}
+
+	c.Set("Connection", "close")
+	c.Set("Server", "Apache")
+	c.Set("Vary", "Accept-Encoding")
+
+	return err
 }
