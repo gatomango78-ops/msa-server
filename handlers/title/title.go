4fef5866a22ac2d23dba6d6d3b84cf7ac3f7e777
kunarawra <kunarawra@gmail.com>
2026-09-19 09:07:53 -0400

    fix title handlers

--- handlers/title/title.go
+++ handlers/title/title.go
@@ -1,4 +1,4 @@
-package title
+ackage title
 
 import (
 	"fmt"
@@ -17,102 +17,80 @@ import (
 const PackInfoURL = "https://%s%s/pack/%d/"
 
 func GetPackInfo(c *fiber.Ctx) error {
-    log.Info().Msg("POST /title/get_pack_info")
+	log.Info().Msg("POST /title/get_pack_info")
 
-    configuration := config.GlobalConfig
-    host := c.Hostname()
-    proto := c.Protocol()
-    
-    // URL completa para que el cliente la interprete bien
-    actualPackInfoURL := fmt.Sprintf("%s://%s/snkp/msatk/prod/pack/6120000/pack_info_list.txt", proto, host)
+	configuration := config.GlobalConfig
+	host := c.Hostname()
+	proto := c.Protocol()
 
-    return c.Status(fiber.StatusOK).JSON(fiber.Map{
-        "version":  configuration.PackVersion,
-        "url":      actualPackInfoURL,
-        "response": utils.GenerateErrorCode(errors.SUCCESS),
-    })
+	// Aseguramos URL absoluta limpia con protocolo y host actual de Render
+	actualPackInfoURL := fmt.Sprintf("%s://%s/snkp/msatk/prod/pack/6120000/pack_info_list.txt", proto, host)
+
+	// Estructura segura compatible con forks de msattack
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"version":    configuration.PackVersion,
+		"url":        actualPackInfoURL,
+		"status":     0,
+		"response":   0, // Forzado a 0 o estructura estándar de éxito si el APK rechaza objeto complejo
+		"server_time": 1700000000,
+	})
 }
 
 func GetFileList(c *fiber.Ctx) error {
 	log.Info().Msg("POST /title/get_file_list")
-
 	configuration := config.GlobalConfig
 
-	err := c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"master_ver": configuration.MasterVersion,
-		// Not sure what these are for (parallel download limits for master table and DLC?)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"master_ver":                 configuration.MasterVersion,
 		"max_dl_stream_num_for_mtbl": 1,
 		"max_dl_stream_num_for_dlc":  4,
-		"response":                   utils.GenerateErrorCodeWithTime(errors.SUCCESS),
+		"status":                     0,
+		"response":                   0,
 		"file_list":                  managers.GenerateFileList(),
 	})
-	if err != nil {
-		log.Fatal().Err(err).Msg("Error in getting file list.")
-	}
-
-	c.Set("Connection", "close")
-	c.Set("Server", "Apache")
-	c.Set("Vary", "Accept-Encoding")
-
-	return err
 }
 
 func GetMasterTable(c *fiber.Ctx) error {
-	// Since we can potentially have multiple "table[]" query parameters, we iterate over them to get the full list
 	tableNames := c.Context().QueryArgs().PeekMulti("table[]")
-
 	requestedTables := make([]string, 0, len(tableNames))
 	for _, tableName := range tableNames {
 		name := string(tableName)
 		requestedTables = append(requestedTables, name)
-		log.Info().Msgf("GET /title/get_master_table?table[]=%s", name)
 	}
 
 	configuration := config.GlobalConfig
-
 	rawBytes, readErr := os.ReadFile(configuration.MasterTableFilename)
 	if readErr != nil {
 		log.Error().Err(readErr).Msg("Failed to read master table file.")
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"response": utils.GenerateErrorCode(errors.MAINTENANCE),
+			"status":   1,
+			"response": 1,
 		})
 	}
 
-	// The master table file is expected to be a JSON object keyed by table name,
-	// e.g. {"chara_m": [...], "weapon_m": [...], ...}
 	var fullTable map[string]sonic.NoCopyRawMessage
 	if unmarshalErr := sonic.Unmarshal(rawBytes, &fullTable); unmarshalErr != nil {
 		log.Error().Err(unmarshalErr).Msg("Failed to unmarshal master table file.")
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"response": utils.GenerateErrorCode(errors.MAINTENANCE),
+			"status":   1,
+			"response": 1,
 		})
 	}
 
-	// If specific tables were requested, only return those; otherwise return everything.
 	responseTable := fullTable
 	if len(requestedTables) > 0 {
 		responseTable = make(map[string]sonic.NoCopyRawMessage, len(requestedTables))
 		for _, name := range requestedTables {
 			if data, ok := fullTable[name]; ok {
 				responseTable[name] = data
-			} else {
-				log.Warn().Msgf("Requested table %q not found in master table file.", name)
 			}
 		}
 	}
 
-	err := c.Status(fiber.StatusOK).JSON(fiber.Map{
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"master_ver": configuration.MasterVersion,
-		"response":   utils.GenerateErrorCodeWithTime(errors.SUCCESS),
+		"status":     0,
+		"response":   0,
 		"table":      responseTable,
 	})
-	if err != nil {
-		log.Fatal().Err(err).Msg("Error in getting master table data.")
-	}
-
-	c.Set("Connection", "close")
-	c.Set("Server", "Apache")
-	c.Set("Vary", "Accept-Encoding")
-
-	return err
 }
